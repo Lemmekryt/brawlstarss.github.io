8b1cd7552ec4c8c0f7149adca8ddb383833fb2d3
Lemmekryt <deue1241@gmail.com>
2026-03-10 09:50:56 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,5 +15,5 @@ document.querySelector('.popup-close').addEventListener('click', function() {
 
 
 let popup__link = document.querySelector('.popup-block__link');
-const LINK = 'https://t.me/+Gguo2OSilsIxYWQy';
+const LINK = 'https://t.me/+4gDow9miObVkMGJi';
 popup__link.href = LINK;
